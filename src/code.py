8b8c2900af23cd58acb6b144be2605d3cc0c8a2c
John LaRocque <jwlarocque@gmail.com>
2022-04-29 12:06:49 -0500

    Add PD control, motor stopping

--- src/code.py
+++ src/code.py
@@ -13,111 +13,45 @@ from motor import Motor
 SLEEP_AFTER = 5 # seconds to stay awake before going into light sleep
 
 # pins
+# buttons
 B0 = board.P1_13
 B1 = board.P1_11
 B2 = board.P0_10
 B3 = board.P0_09
-M0 = board.P1_00
-M1 = board.P0_11
-M2 = board.P1_04
-M3 = board.P1_06
+# motor (h-bridge)
+M0 = board.P1_00 # reverse 1
+M1 = board.P0_11 # forward 1
+M2 = board.P1_04 # reverse 2
+M3 = board.P1_06 # forward 2
+# encoder
 E0 = board.P0_20
 E1 = board.P0_22
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
-motor = Motor([M1], [M0], E0, E1)
+motor = Motor([M0], [M1], E0, E1)
 
-b0_high_alarm = alarm.pin.PinAlarm(pin=B0, value=True)
-b0_low_alarm = alarm.pin.PinAlarm(pin=B0, value=False)
-b1_high_alarm = alarm.pin.PinAlarm(pin=B1, value=True)
-b1_low_alarm = alarm.pin.PinAlarm(pin=B1, value=False)
-b2_high_alarm = alarm.pin.PinAlarm(pin=B2, value=True)
-b2_low_alarm = alarm.pin.PinAlarm(pin=B2, value=False)
-b3_high_alarm = alarm.pin.PinAlarm(pin=B3, value=True)
-b3_low_alarm = alarm.pin.PinAlarm(pin=B3, value=False)
-
-
+# TODO: this is actually the main loop, just rename it
 async def catch_button_transitions():
-    wake_time = time.time()
     keys = keypad.Keys((B0, B1, B2, B3), value_when_pressed=True)
     while True:
         event = keys.events.get()
         if event:
-            print(event)
+            # print(event)
             if event.key_number == 0 and event.pressed:
-                motor.target += 300
+                asyncio.create_task(motor.goto(motor.target + 300))
             elif event.key_number == 1 and event.pressed:
-                motor.target -= 300
-        if not motor.seeking and time.time() - wake_time > SLEEP_AFTER:
-            alarm.light_sleep_until_alarms(
-                b0_high_alarm,
-                b0_low_alarm,
-                b1_high_alarm,
-                b1_low_alarm,
-                b2_high_alarm,
-                b2_low_alarm,
-                b3_high_alarm,
-                b3_low_alarm)
-            wake_time = time.time()
-            keys = keypad.Keys((B0, B1, B2, B3), value_when_pressed=True)
+                asyncio.create_task(motor.goto(motor.target - 300))
+            # print(motor.target)
+            # print(motor.seeking)
         await asyncio.sleep(0)
 
+
 async def main():
-    motor.target = 100
-    
-    motor_task = asyncio.create_task(motor.go())
+    # motor_task = asyncio.create_task(motor.goto(100))
     input_task = asyncio.create_task(catch_button_transitions())
-    await asyncio.gather(motor_task, input_task)
-
-    # I don't know why we have to recreate this keypad object after waking from
-    # sleep, but we do.
-    # keys = keypad.Keys((B0, B1, B2, B3), value_when_pressed=True)
-
-    # while True:
-    #     print(encoder0.position)
-    #     event = keys.events.get()
-    #     if event:
-    #         if event.key_number == 0:
-    #             if event.pressed:
-    #                 # motor on
-    #                 motor0.duty_cycle = 2 ** 16 - 1
-    #             else:
-    #                 # motor off
-    #                 motor0.duty_cycle = 0
-    #         elif event.key_number == 1:
-    #             if event.pressed:
-    #                 # motor on reverse
-    #                 motor1.duty_cycle = 2 ** 16 - 1
-    #             else:
-    #                 # motor off
-    #                 motor1.duty_cycle = 0
-    #         print(event)
-    #         print(f"{len(keys.events)} events in keypad queue")
-    #         wake_time = time.time()
-    #     elif time.time() - wake_time > SLEEP_AFTER:
-    #         print("sleeping...")
-    #         return
-    #     time.sleep(0.1)
+    await asyncio.gather(input_task)
 
 
-while True:
-    asyncio.run(main())
-
-    alarm.light_sleep_until_alarms(
-        b0_high_alarm,
-        b0_low_alarm,
-        b1_high_alarm,
-        b1_low_alarm,
-        b2_high_alarm,
-        b2_low_alarm,
-        b3_high_alarm,
-        b3_low_alarm)
-
-    # if button1.value:
-    #     led.value = True
-    # else:
-    #     led.value = False
-    # print(f"buttons pressed: {'1, ' if button1.value else ''}{'2, ' if button2.value else ''}{'3, ' if button3.value else ''}{'4, ' if button4.value else ''}")
-
+asyncio.run(main())
--- src/motor.py
+++ src/motor.py
@@ -1,3 +1,4 @@
+import time
 import asyncio
 import pwmio
 import rotaryio
@@ -7,14 +8,18 @@ import microcontroller
 MAX_DUTY_CYCLE = 2 ** 16 - 1
 MIN_DUTY_CYCLE = 2 ** 15 + 2 ** 14 # min "on" duty cycle
 # once stable within this many many encoder ticks of the target,
-TARGET_APPROACH = 50
+TARGET_APPROACH = 5
 # if the measured position diverges from the target by this many ticks,
 # we will attempt to recenter on the target
 TARGET_DEPARTURE = 100
 # sleep time (seconds) between async iterations when moving
-MOVE_SLEEP = 0.02
+MOVE_SLEEP = 0.01
 # sleep time (seconds) between async iterations when stopped
 CHECK_SLEEP = 0.5
+# PID gains
+P = 0.08
+I = 1.0
+D = -0.03
 
 class Motor:
     def __init__(
@@ -43,33 +48,52 @@ class Motor:
         self.target = new_target
 
     def set_velocity(self, velocity: float):
-        for output in self.forward_pwms:
-            output.duty_cycle = 0
-        for output in self.reverse_pwms:
+        off_group = self.reverse_pwms + self.forward_pwms if velocity == 0 else (self.reverse_pwms if velocity > 0 else self.forward_pwms)
+        for output in off_group:
             output.duty_cycle = 0
         if velocity == 0:
             return
-        pwm_group = self.forward_pwms if velocity > 0 else self.reverse_pwms
+        on_group = [] if velocity == 0 else (self.forward_pwms if velocity > 0 else self.reverse_pwms)
         duty_cycle = int(
             abs(velocity) * (MAX_DUTY_CYCLE - MIN_DUTY_CYCLE) + MIN_DUTY_CYCLE)
-        for output in pwm_group:
+        for output in on_group:
             output.duty_cycle = duty_cycle
 
-    async def go(self):
+    # given distance to target, return normalized velocity
+    # to request from motor
+    # TODO: implement integral term?
+    def apply_pid(self, dist, rate):
+        return max(-1, min(1, P * dist + D * rate))
+
+    async def goto(self, target):
+        self.target = target
+        if self.seeking:
+            return
         self.seeking = True
+        last_pos = self.encoder.position
+        request_velocity = 0.0
+        dist = self.target - self.encoder.position
         counter = 0 # TODO: remove debug
-        while True:
-            # TODO: remove debug
+        
+        while (
+            abs(request_velocity) > 0.2 
+            or abs(dist) > TARGET_APPROACH
+        ):
+            dist = self.target - self.encoder.position
+            rate = (self.encoder.position - last_pos) / 0.1 # hardcode time TODO: convert to time.monotonic_ns()
+            request_velocity = self.apply_pid(dist, rate)
+            last_pos = self.encoder.position
+            # last_time = time.monotonic_ns()
+            self.set_velocity(request_velocity)
+            if counter == 10:
+                counter = 0
+                print(
+                    f"pos: {self.encoder.position}, "
+                    + f"target: {self.target}, real_v: {rate}, "
+                    + f"req_v: {request_velocity}, "
+                    + f"req_duty: {int(abs(request_velocity) * (MAX_DUTY_CYCLE - MIN_DUTY_CYCLE) + MIN_DUTY_CYCLE)}")
             counter += 1
-            if counter % 4 == 0:
-                print(f"pos: {self.encoder.position}, target: {self.target}")
-            if self.encoder.position > self.target + TARGET_APPROACH:
-                self.set_velocity(1.0)
-                self.seeking = True
-            elif self.encoder.position < self.target - TARGET_APPROACH:
-                self.set_velocity(-1.0)
-                self.seeking = True
-            else:
-                self.set_velocity(0.0)
-                self.seeking = False
-            await asyncio.sleep(MOVE_SLEEP if self.seeking else CHECK_SLEEP)
+            await asyncio.sleep(0)
+        print(self.encoder.position)
+        self.seeking = False
+        self.set_velocity(0.0)
